f94ce6d273671003e1f9c0fc9c298a8aa3e17c97
nisanurs <niisanur.st@gmail.com>
2026-01-29 19:46:13 +0300

    update

--- src/components/VenueDetail.jsx
+++ src/components/VenueDetail.jsx
@@ -1,26 +1,26 @@
 import React, { useState, useEffect } from "react";
-import { useParams, NavLink } from "react-router-dom"; 
+import { useParams, NavLink } from "react-router-dom";
 import Rating from "./Rating";
 import FoodAndDrinkList from "./FoodAndDrinkList";
 import Header from "./Header";
 import HourList from "./HourList";
 import CommentList from "./CommentList";
-import VenueDataService from "../services/VenueDataService"; 
+import VenueDataService from "../services/VenueDataService";
 
 const VenueDetail = () => {
-  const [venue, setVenue] = useState(null); 
+  const [venue, setVenue] = useState(null);
   const { id } = useParams();
 
   useEffect(() => {
-    // URL'deki ID'nin sonundaki ":1" gibi hataları temizler
+    // ID sonundaki :1 hatasini temizle
     const cleanId = id && id.includes(':') ? id.split(':')[0] : id;
-    
+
     VenueDataService.getVenue(cleanId)
       .then((response) => {
-        setVenue(response.data); 
+        setVenue(response.data);
       })
       .catch((e) => {
-        console.error("Veri çekme hatası:", e);
+        console.error("Veri cekme hatasi:", e);
       });
   }, [id]);
 
@@ -33,29 +33,53 @@ const VenueDetail = () => {
       <Header headerText={venue.name} />
       <div className="container">
         <div className="row">
-          <div className="col-xs-12">
+          <div className="col-xs-12 col-md-12">
             <div className="row">
+              {/* MEKAN BILGILERI */}
               <div className="col-xs-12 col-sm-6">
                 <p className="rating"><Rating rating={venue.rating} /></p>
                 <p>{venue.address}</p>
                 <div className="panel panel-primary">
                   <div className="panel-heading"><h2 className="panel-title">Calisma Saatleri</h2></div>
-                  <div className="panel-body"><HourList hourList={venue.hours || []} /></div>
+                  <div className="panel-body">
+                    <HourList hourList={venue.hours || []} />
+                  </div>
                 </div>
                 <div className="panel panel-primary">
                   <div className="panel-heading"><h2 className="panel-title">Neler Var?</h2></div>
-                  <div className="panel-body"><FoodAndDrinkList foodAndDrinkList={venue.foodanddrink || []} /></div>
+                  <div className="panel-body">
+                    <FoodAndDrinkList foodAndDrinkList={venue.foodanddrink || []} />
+                  </div>
                 </div>
               </div>
+
+              {/* HARITA BOLUMU */}
               <div className="col-xs-12 col-sm-6">
                 <div className="panel panel-primary">
                   <div className="panel-heading"><h2 className="panel-title">Konum Bilgisi</h2></div>
                   <div className="panel-body">
                     {venue.coordinates && (
                       <img className="img-responsive img-rounded" alt="Konum"
-                        src={`https://maps.googleapis.com/maps/api/staticmap?center=${venue.coordinates[1]},${venue.coordinates[0]}&zoom=15&size=600x400&markers=${venue.coordinates[1]},${venue.coordinates[0]}&key=BURAYA_API_KEY`}
+                        src={`https://maps.googleapis.com/maps/api/staticmap?center=${venue.coordinates[1]},${venue.coordinates[0]}&zoom=15&size=600x400&markers=${venue.coordinates[1]},${venue.coordinates[0]}&key=AIzaSyCmmKygTpBzHGOZEciJpAdxC9v_tWHagnE`}
                       />
                     )}
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* YORUMLAR BOLUMU */}
+            <div className="row">
+              <div className="col-xs-12">
+                <div className="panel panel-primary">
+                  <div className="panel-heading">
+                    <NavLink className="btn btn-default pull-right" to={`/venue/${id}/comment/new`} state={{ name: venue.name }}>
+                      Yorum Ekle
+                    </NavLink>
+                    <h2 className="panel-title">Yorumlar</h2>
+                  </div>
+                  <div className="panel-body">
+                    <CommentList commentList={venue.comments || []} />
                   </div>
                 </div>
               </div>
